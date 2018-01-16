c255ede6bda27acf0730a8911248ac746ea61d37
Unknown <alexisbln44@gmail.com>
2018-01-16 14:26:47 +0100

    modif du chemin d'acces java

--- db/_startServer.bat
+++ db/_startServer.bat
@@ -1,5 +1,5 @@
 set HSQLDB_LIB=hsqldb-1.8.0.7.jar
 
-"C:\Program Files\Java\jdk1.8.0_152\bin\java.exe" -classpath %HSQLDB_LIB% org.hsqldb.Server -database.0 file:projet1 -port 9003
+"java" -classpath %HSQLDB_LIB% org.hsqldb.Server -database.0 file:projet1 -port 9003
 
 pause
